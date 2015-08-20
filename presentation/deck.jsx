2d001452e1f0b5303f180a48fc1ae40171ff1be9
Alexander Suevalov <suevalov.work@gmail.com>
2015-08-20 15:16:01 +0300

    Working on presentation

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -2,19 +2,24 @@ import React from 'react/addons';
 
 import {
   BlockQuote, Cite, CodePane, Deck, Fill,
-  Heading, Image, Layout, ListItem, List, Quote, Slide
+  Heading, Image, Layout, ListItem, List, Quote, Slide, Text
 } from '../src/spectacle';
 
 import preloader from '../src/utils/preloader';
 
 import Interactive from './components/interactive';
 import Suevalov from './components/suevalov';
+import Rectangle from './components/rectangle';
 
 const images = {
   thanks: require('./images/ironman-thanks.gif'),
   logo: require('./images/electron-logo.png'),
+  nwLogo: require('./images/nw-logo.png'),
   me: require('./images/me.png'),
-  timBernens: require('./images/tim-bernens.jpg')
+  timBernens: require('./images/tim-bernens.jpg'),
+  distribution: require('./images/distribution.png'),
+  prefer: require('./images/prefer.jpg'),
+  electronApps: require('./images/electron-apps.png')
 };
 
 preloader([images.me, images.timBernens, images.logo, images.thanks]);
@@ -85,6 +90,58 @@ export default class extends React.Component {
                 <ListItem>IoT</ListItem>
                 <ListItem>Any combination of the above</ListItem>
             </List>
+        </Slide>
+        <Slide bgColor='primary'>
+            <Image src={images.distribution} width='90%' />
+        </Slide>
+        <Slide bgColor='primary'>
+            <Layout>
+                <Fill>
+                    <div style={{'margin-top': '130px'}}>
+                        <Rectangle>
+                            Shared code
+                        </Rectangle>
+                    </div>
+                </Fill>
+                <Fill>
+                    <Rectangle>
+                        Desktop code
+                    </Rectangle>
+                    <Rectangle>
+                        Web code
+                    </Rectangle>
+                </Fill>
+            </Layout>
+        </Slide>
+        <Slide bgColor='primary'>
+            <Layout>
+                <Fill>
+                    <Image src={images.logo} width='200px' />
+                    <a href='http://electron.atom.io' target='_blank'>
+                        <Text textColor='darkPrimary'>Electron.js</Text>
+                    </a>
+                </Fill>
+                <Fill>
+                    <Image src={images.nwLogo} width='200px' />
+                    <a href='http://nwjs.io/' target='_blank'>
+                        <Text textColor='darkPrimary'>NW.js</Text>
+                    </a>
+                </Fill>
+            </Layout>
+            <a href='https://github.com/atom/electron/blob/master/docs/development/atom-shell-vs-node-webkit.md' target='_blank' style={{ 'font-size': '10px', 'margin-top': '100px', 'display': 'block'}}>
+                <Text textColor='darkPrimary'>Electron's tech differences to NW.js</Text>
+            </a>
+
+        </Slide>
+        <Slide bgColor='white'>
+            <Heading size={4} textColor='darkPrimary'>Notable applications</Heading>
+            <Image src={images.electronApps} />
+            <a href='https://github.com/nwjs/nw.js/wiki/List-of-apps-and-companies-using-nw.js' target='_blank' style={{ 'font-size': '10px', 'margin-top': '100px', 'display': 'block'}}>
+                <Text textColor='darkPrimary'>NW.js apps list</Text>
+            </a>
+            <a href='https://github.com/sindresorhus/awesome-electron' target='_blank' style={{ 'font-size': '10px', 'margin-top': '10px', 'display': 'block'}}>
+                <Text textColor='darkPrimary'>Electron apps list</Text>
+            </a>
         </Slide>
         <Slide bgColor='primary' notes='<ul><li>talk about that</li><li>and that</li></ul>'>
           <CodePane
